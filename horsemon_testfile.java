e03e7ca048b998d70353c158c2de33f60a34fa87
Aziquesa <aziquesa@live.com>
2018-04-15 14:07:28 +0200

    Initial commit

---
+++ horsemon_testfile.java
@@ -0,0 +1,9 @@
+public class HorsemonGO{
+    private String breed;
+    private String gender;
+
+    public static void main(String[] args){
+        System.out.println("Welcome to the game!");
+    }
+
+}
